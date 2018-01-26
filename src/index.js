7923fecf8d04bd30c23d6f01bb2bcb12195997d1
m31271n <m31271n@users.noreply.github.com>
2018-01-27 00:55:31 +0800

    feat: adjust example code

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { useStrict } from 'mobx';
+
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
