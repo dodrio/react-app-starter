109c7821ac0f6b426414b28714849065a928dfba
m31271n <m31271n@gmail.com>
2018-01-27 00:09:47 +0800

    feat: add styled-components and polished support

--- config-overrides.js
+++ config-overrides.js
@@ -1,8 +1,12 @@
 const rewireEslint = require('react-app-rewire-eslint');
+const rewireStyledComponents = require('react-app-rewire-styled-components');
+const rewirePolished = require('react-app-rewire-polished');
 const rewireMobx = require('react-app-rewire-mobx');
 
 module.exports = function override(config, env) {
   config = rewireEslint(config, env, options => (options.baseConfig = {}));
+  config = rewireStyledComponents(config, env);
+  config = rewirePolished(config, env);
   config = rewireMobx(config, env);
 
   return config;
