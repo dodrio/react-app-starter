ddb6a0465be8701d9ca9c046b439ab2f7c1a26e7
m31271n <m31271n@users.noreply.github.com>
2018-01-26 21:29:13 +0800

    feat: add mobx in strict-mode

--- config-overrides.js
+++ config-overrides.js
@@ -1,7 +1,9 @@
 const rewireEslint = require('react-app-rewire-eslint');
+const rewireMobx = require('react-app-rewire-mobx');
 
 module.exports = function override(config, env) {
   config = rewireEslint(config, env, options => (options.baseConfig = {}));
+  config = rewireMobx(config, env);
 
   return config;
 };
--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { useStrict } from 'mobx';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+
+// enable strict-mode of mobx
+useStrict(true);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
