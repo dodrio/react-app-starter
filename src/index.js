9c8055bc70da0874c79ee9054ad2967a6635106e
m31271n <m31271n@gmail.com>
2018-01-27 01:02:06 +0800

    feat: add promise.prototype.finally support

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { useStrict } from 'mobx';
+import promiseFinally from 'promise.prototype.finally';
 
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+
+// polyfill promise finally
+promiseFinally.shim();
 
 // enable strict-mode of mobx
 useStrict(true);
