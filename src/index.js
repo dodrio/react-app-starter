74852d06c5bb0579f36203469ec9dddf7ebdf883
m31271n <m31271n@users.noreply.github.com>
2018-01-27 09:12:29 +0800

    feat: rewrite example code with mobx

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { useStrict } from 'mobx';
+import { Provider } from 'mobx-react';
 import promiseFinally from 'promise.prototype.finally';
+import registerServiceWorker from './registerServiceWorker';
+
+import counterStore from './stores/counter';
 
 import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import App from './components/App';
 
 // polyfill promise finally
 promiseFinally.shim();
@@ -13,5 +17,17 @@ promiseFinally.shim();
 // enable strict-mode of mobx
 useStrict(true);
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const stores = {
+  counterStore,
+};
+
+ReactDOM.render(
+  <Provider {...stores}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
+  document.getElementById('root')
+);
+
 registerServiceWorker();
