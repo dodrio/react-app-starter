98d2acd6ca440a7e6deb0b808d5b00a868921dd4
m31271n <m31271n@users.noreply.github.com>
2018-02-06 12:44:29 +0800

    feat: remove polished

--- config-overrides.js
+++ config-overrides.js
@@ -1,13 +1,11 @@
 const rewireEslint = require('react-app-rewire-eslint');
 const rewireStyledComponents = require('react-app-rewire-styled-components');
-const rewirePolished = require('react-app-rewire-polished');
 const rewireMobx = require('react-app-rewire-mobx');
 const rewireReactHotLoader = require('react-app-rewire-hot-loader');
 
 module.exports = function override(config, env) {
   config = rewireEslint(config, env, options => (options.baseConfig = {}));
   config = rewireStyledComponents(config, env);
-  config = rewirePolished(config, env);
   config = rewireMobx(config, env);
   config = rewireReactHotLoader(config, env);
 
