a42acb51fa85ae125e8d76e3036600acf16687a2
m31271n <m31271n@users.noreply.github.com>
2018-05-12 00:05:28 +0800

    refactor: structure config-overrides according official docs

--- config-overrides/index.js
+++ config-overrides/index.js
@@ -2,8 +2,8 @@ const rewireEslint = require('react-app-rewire-eslint');
 const rewireStyledComponents = require('react-app-rewire-styled-components');
 const rewireBabelPolyfill = require('react-app-rewire-polyfills');
 const rewireReactHotLoader = require('react-app-rewire-hot-loader');
-const loadBuildInfo = require('./config-overrides.d/build-info');
-const rewireCriticalPlugin = require('./config-overrides.d/rewire-critical-plugin');
+const loadBuildInfo = require('./build-info');
+const rewireCriticalPlugin = require('./rewire-critical-plugin');
 
 loadBuildInfo();
 
