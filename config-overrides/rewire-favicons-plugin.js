43e7fd3469591a2bb586a002008ae064b4eba540
m31271n <m31271n@users.noreply.github.com>
2018-05-12 15:53:59 +0800

    feat: add support for generating multiple favicons

---
+++ config-overrides/rewire-favicons-plugin.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const FaviconsPlugin = require('favicons-webpack-plugin');
+
+function rewireFaviconnsPlugin(config, env) {
+  config.plugins = (config.plugins || []).concat(
+    new FaviconsPlugin({
+      logo: path.resolve(__dirname, '../public/logo.png'),
+      prefix: 'icons-[hash]/',
+      inject: true,
+      persistentCache: true,
+      icons: {
+        android: true,
+        favicons: true,
+        appleIcon: true,
+        appleStartup: true,
+        coast: false,
+        firefox: false,
+        opengraph: false,
+        twitter: false,
+        yandex: false,
+        windows: false,
+      },
+    })
+  );
+
+  return config;
+}
+
+module.exports = rewireFaviconnsPlugin;
