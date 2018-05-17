93a9f8961b8e60e20eba48fc9579793722b61822
m31271n <m31271n@users.noreply.github.com>
2018-05-17 10:46:27 +0800

    feat: rewire babel-loader simply
    Solution at https://github.com/dashed/react-app-rewire-babel-loader
    didn't work for me. so write a simple one.

--- config-overrides/index.js
+++ config-overrides/index.js
@@ -5,6 +5,7 @@ const rewireReactHotLoader = require('react-app-rewire-hot-loader');
 const loadBuildInfo = require('./build-info');
 const rewireCriticalPlugin = require('./rewire-critical-plugin');
 const rewireFaviconsPlugin = require('./rewire-favicons-plugin');
+const rewireBabelLoader = require('./rewire-babel-loader');
 
 loadBuildInfo();
 
@@ -14,6 +15,9 @@ module.exports = function override(config, env) {
   config = rewireBabelPolyfill(config, env);
   config = rewireReactHotLoader(config, env);
   config = rewireFaviconsPlugin(config, env);
+  config = rewireBabelLoader(config, env, [
+    // example: 'node_modules/base-scale'
+  ]);
 
   if (env === 'production') {
     config = rewireCriticalPlugin(config, env);
