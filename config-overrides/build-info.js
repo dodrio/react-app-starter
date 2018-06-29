1cdcdbae9e80401ef7e8a20589ce7de33f7b864d
m31271n <m31271n@users.noreply.github.com>
2018-06-29 11:29:19 +0800

    fix: fix error when .git is not created

--- config-overrides/build-info.js
+++ config-overrides/build-info.js
@@ -5,14 +5,20 @@ const childProcess = require('child_process')
 
 function loadMetaInfo() {
   const version = require('../package.json').version
-  const commit = childProcess
-    .execSync('git rev-parse --short HEAD')
-    .toString()
-    .trim()
+
+  let commit = 'no commit'
+  try {
+    commit = childProcess
+      .execSync('git rev-parse --short HEAD')
+      .toString()
+      .trim()
+  } catch (e) {
+    console.error('[build info] failed to fetch commit number')
+  }
+
+  const UTCDateTime = new Date().toISOString()
 
   process.env.REACT_APP_BUILD_VERSION = version
   process.env.REACT_APP_BUILD_COMMIT = commit
-  process.env.REACT_APP_BUILD_DATETIME = new Date().toISOString()
+  process.env.REACT_APP_BUILD_DATETIME = UTCDateTime
 }
-
-module.exports = loadMetaInfo
